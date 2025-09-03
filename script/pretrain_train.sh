83fdb639b20282fbe8c8215906a71439c7aba29f
llzllx <llzllx70@163.com>
2025-09-03 15:06:42 +0800

    modify

--- script/pretrain_train.sh
+++ script/pretrain_train.sh
@@ -2,4 +2,4 @@
 #!/bin/bash
 
 
-nohup python -m src.trainer.MyPretrainTrainer --task train --model Qwen3-4B-Base --step 50 > pretrain_train.out 2>&1 &
+nohup python -m src.trainer.MyPretrainTrainer --task train --model Qwen3-4B-Base --step 20 > pretrain_train.out 2>&1 &
