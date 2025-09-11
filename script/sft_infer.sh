f8039056a414db64257acdbb7e2d5a7d0dee892a
llzllx70 <llzllx70@163.com>
2025-09-11 14:06:26 +0800

    modify

--- script/sft_infer.sh
+++ script/sft_infer.sh
@@ -4,5 +4,5 @@
 
 # nohup python -m src.trainer.MySFTTrainer --task infer --model Qwen3-4B-Base > infer.out 2>&1 &
 
-python -m src.trainer.MySFTTrainer --task infer
+python -m src.infer.MyInfer --task sft
 
